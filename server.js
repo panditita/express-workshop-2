44241a6963e1b9e7e9272bb1ea1008f6b5fdbb18
panditita <topimorita@gmail.com>
2017-07-29 15:58:16 +0100

    Website example with node and server

--- server.js
+++ server.js
@@ -1,10 +1,65 @@
 const express = require("express");
 const app = express();
+const exphbs = require('express-handlebars');
+const fs = require("fs");
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
+app.use(express.static("public", { 'extensions': ['html'] }));
 
+// app.get (path, handle)
+/* app.get('/', function (req, res) {
+  res.render("index", {
+    title: 'Etzali Hernández', // insert your name instead 
+      subheading: "photography portfolio"
+
+  });
+
+}); */
+
+app.get('/my-cv', function (req, res) {
+  res.render("my-cv", {
+    //title: 'Etzalis Profile', // insert your name instead 
+    subheading: "updated cv",
+
+  });
+});
+
+app.get('/admin', function (req, res) {
+  res.render("admin", {
+    title: 'Admin', // insert your name instead 
+        subheading: "updates on admin work"
+
+  });
+});
+
+app.get('/contact', function (req, res) {
+  res.render("contact", {
+    title: 'Get in Touch', // insert your name instead 
+    subheading: "fill the form"
+  });
+});
+
+app.get('/', function (req, res){ 
+    const filePath = __dirname + '/data/posts.json'; 
+    const callbackFunction = function(error, file){
+        // we call .toString() to turn the file buffer to a String 
+    const fileData = file.toString();
+        // we use JSON.parse to get an object out the String 
+    const postsJson = JSON.parse(fileData);
+        // send the json to the Template to render 
+        res.render('index',
+        { 
+            title: 'Etzali Profile',
+            // insert your name instead 
+            posts: postsJson,
+        });
+    };
+             fs.readFile(filePath, callbackFunction);
+});
 
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
